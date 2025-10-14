e136f38734758cccd2e691edd334db3357b6ab0d
Erfankhodami <khodami.erfan@gmail.com>
2025-10-14 19:45:01 +0330

    added a working swagger

---
+++ WebApp2/Controllers/MostBasicController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers;
+
+[ApiController]
+[Route("BasicController")]
+public class MostBasicController : ControllerBase
+{
+    [HttpGet]
+    public string GetString()
+    {
+        return "Hello World";
+    }
+}
